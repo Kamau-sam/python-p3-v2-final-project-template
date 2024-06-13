d4932fc2def1c1bde001f12fd5c893465e961f3c
Kamau-sam <skamau.6411@gmail.com>
2024-06-13 19:06:59 +0300

    created the helper.py

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,9 +1,31 @@
-# lib/helpers.py
+# lib/database/db_helper.py
 
-def helper_1():
-    print("Performing useful function#1.")
+import sqlite3
 
+DB_NAME = 'medical_management.db'
 
-def exit_program():
-    print("Goodbye!")
-    exit()
+def get_connection():
+    conn = sqlite3.connect(DB_NAME)
+    return conn
+
+def execute_query(query, params=()):
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute(query, params)
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"Error executing query: {e}")
+
+def fetch(query, params=()):
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute(query, params)
+        result = cursor.fetchall()
+        conn.close()
+        return result
+    except sqlite3.Error as e:
+        print(f"Error fetching data: {e}")
+        return []
