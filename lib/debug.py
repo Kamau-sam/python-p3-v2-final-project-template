e955986512d4d1cc26171b4c561700ec12f9b4f4
Kamau-sam <skamau.6411@gmail.com>
2024-06-13 19:21:57 +0300

    updated the debug model

--- lib/debug.py
+++ lib/debug.py
@@ -1,8 +1,12 @@
-#!/usr/bin/env python3
-# lib/debug.py
 
-from models.__init__ import CONN, CURSOR
-import ipdb
+from models.appointment import Appointment
 
+appointment = Appointment()
 
-ipdb.set_trace()
+def main():
+    appointments = appointment.all()
+    for appt in appointments:
+        print(f'Appointment {appt[0]}: Pet {appt[1]}, Date {appt[2]}, Status {appt[3]}, Description {appt[4]}')
+
+if __name__ == "__main__":
+    main()
