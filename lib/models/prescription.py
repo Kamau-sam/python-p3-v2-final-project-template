59d6932e1b7295c9e6ec1af77c7d0f1669814e8a
Kamau-sam <skamau.6411@gmail.com>
2024-06-13 20:22:55 +0300

    updated the prescription model

--- lib/models/prescription.py
+++ lib/models/prescription.py
@@ -1,52 +1,22 @@
+class Prescription:
+    def __init__(self, id, patient_id, doctor_id, medication, instructions):
+        self.id = id
+        self.patient_id = patient_id
+        self.doctor_id = doctor_id
+        self.medication = medication
+        self.instructions = instructions
 
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+    @staticmethod
+    def get_all_prescriptions():
+        pass
 
-from database.db_helper import execute_query, fetch
+    @staticmethod
+    def get_prescription_by_id(prescription_id):
+        pass
 
-class MedicalRecord:
-    def create_table(self):
-        query = '''
-        CREATE TABLE IF NOT EXISTS medical_record (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            patient_id INTEGER,
-            doctor_id INTEGER,
-            diagnosis TEXT,
-            prescription TEXT,
-            FOREIGN KEY (patient_id) REFERENCES patient(id),
-            FOREIGN KEY (doctor_id) REFERENCES doctor(id)
-        );
-        '''
-        execute_query(query)
+    @staticmethod
+    def create_prescription(patient, doctor, medication, instructions):
+        pass
 
-    def drop_table(self):
-        query = "DROP TABLE IF EXISTS medical_record;"
-        execute_query(query)
-
-    def create(self, patient_id, doctor_id, diagnosis, prescription):
-        query = '''
-        INSERT INTO medical_record (patient_id, doctor_id, diagnosis, prescription) 
-        VALUES (?, ?, ?, ?);
-        '''
-        execute_query(query, (patient_id, doctor_id, diagnosis, prescription))
-
-    def all(self):
-        query = "SELECT * FROM medical_record;"
-        return fetch(query)
-
-    def find_by_id(self, record_id):
-        query = "SELECT * FROM medical_record WHERE id = ?;"
-        return fetch(query, (record_id,))
-
-    def update_diagnosis(self, record_id, new_diagnosis):
-        query = "UPDATE medical_record SET diagnosis = ? WHERE id = ?;"
-        execute_query(query, (new_diagnosis, record_id))
-
-    def update_prescription(self, record_id, new_prescription):
-        query = "UPDATE medical_record SET prescription = ? WHERE id = ?;"
-        execute_query(query, (new_prescription, record_id))
-
-    def delete_record(self, record_id):
-        query = "DELETE FROM medical_record WHERE id = ?;"
-        execute_query(query, (record_id,))
+    def delete_prescription(self):
+        pass
