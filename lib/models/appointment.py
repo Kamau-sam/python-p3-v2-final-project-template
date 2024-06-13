d5b75b856e175aa87e707709bd7a68e4cabf96b3
Kamau-sam <skamau.6411@gmail.com>
2024-06-13 20:19:50 +0300

    updated the apointment model

--- lib/models/appointment.py
+++ lib/models/appointment.py
@@ -1,39 +1,23 @@
 
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from database.db_helper import execute_query, fetch
-
 class Appointment:
-    def create_table(self):
-        query = '''
-        CREATE TABLE IF NOT EXISTS appointment (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            pet_id INTEGER,
-            appointment_date TIMESTAMP NOT NULL,
-            status TEXT NOT NULL,
-            description TEXT,
-            FOREIGN KEY (pet_id) REFERENCES pet(id)
-        );
-        '''
-        execute_query(query)
+    def __init__(self, id, patient_id, doctor_id, date, time):
+        self.id = id
+        self.patient_id = patient_id
+        self.doctor_id = doctor_id
+        self.date = date
+        self.time = time
 
-    def drop_table(self):
-        query = "DROP TABLE IF EXISTS appointment;"
-        execute_query(query)
+    @classmethod
+    def get_all_appointments(cls):
+        pass
 
-    def create(self, pet_id, appointment_date, status, description):
-        query = '''
-        INSERT INTO appointment (pet_id, appointment_date, status, description) 
-        VALUES (?, ?, ?, ?);
-        '''
-        execute_query(query, (pet_id, appointment_date, status, description))
+    @classmethod
+    def get_appointment_by_id(cls, appointment_id):
+        pass
 
-    def all(self):
-        query = "SELECT * FROM appointment;"
-        return fetch(query)
+    @classmethod
+    def create_appointment(cls, patient, doctor, date, time):
+        pass
 
-    def find_by_id(self, appointment_id):
-        query = "SELECT * FROM appointment WHERE id = ?;"
-        return fetch(query, (appointment_id,))
+    def delete_appointment(self):
+        pass
