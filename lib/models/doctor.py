512e8ad6bc3ac977b4330aa03848471dc5b082e1
Kamau-sam <skamau.6411@gmail.com>
2024-06-13 20:18:50 +0300

    updated the doctor model

--- lib/models/doctor.py
+++ lib/models/doctor.py
@@ -1,36 +1,26 @@
 
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from database.db_helper import execute_query, fetch
-
 class Doctor:
-    def create_table(self):
-        query = '''
-        CREATE TABLE IF NOT EXISTS doctor (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            specialty TEXT
-        );
-        '''
-        execute_query(query)
+    def __init__(self, id, first_name, last_name, specialization):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.specialization = specialization
 
-    def drop_table(self):
-        query = "DROP TABLE IF EXISTS doctor;"
-        execute_query(query)
+    @classmethod
+    def get_all_doctors(cls):
+        pass
 
-    def create(self, name, specialty):
-        query = '''
-        INSERT INTO doctor (name, specialty) 
-        VALUES (?, ?);
-        '''
-        execute_query(query, (name, specialty))
+    @classmethod
+    def get_doctor_by_name(cls, name):
+        pass
 
-    def all(self):
-        query = "SELECT * FROM doctor;"
-        return fetch(query)
+    @classmethod
+    def get_doctor_by_id(cls, doctor_id):
+        pass
 
-    def find_by_id(self, doctor_id):
-        query = "SELECT * FROM doctor WHERE id = ?;"
-        return fetch(query, (doctor_id,))
+    @classmethod
+    def create(cls, first_name, last_name, specialization):
+        pass
+
+    def delete_doctor(self):
+        pass
